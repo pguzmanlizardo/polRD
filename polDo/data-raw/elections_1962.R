ed6660abd5a3bc5b5160a4e89de5f892da093305
Pablo M. Guzman Lizardo <42866615+pguzmanlizardo@users.noreply.github.com>
2020-08-12 18:06:53 -0400

    Added pkg repository/Added elections_1962
    I added the basic package repository structure to the PolRD folder, under the name polDo. That is where all the package work will be done.
    
    I also added the data for the 1962 elections and started working on it. So far I have changed the column names.

---
+++ polDo/data-raw/elections_1962.R
@@ -0,0 +1,34 @@
+# This scripts reads and tidies the electoral data for 1962
+# First, we have to load the necessary packages
+stopifnot(requireNamespace("readr", quietly = TRUE))
+stopifnot(requireNamespace("stringr", quietly = TRUE))
+stopifnot(requireNamespace("dplyr", quietly = TRUE))
+
+# Load the raw data for the 1962 election
+elections_1962 <- readr::read_csv("data-raw/datasets/elecciones_1962_raw.csv",
+                                  col_names = FALSE)
+#View(elections_1962)
+
+# Assign the correct names to the columns. Which as of now represent the names
+# of the parties that participated in these elections
+names(elections_1962) <- elections_1962[1:4, ] %>%
+  lapply(paste, collapse = " ") %>%
+  stringr::str_replace_all(pattern = "(NA NA|^NA )", replacement = "") %>%
+  stringr::str_trim() %>%
+  unique() %>%
+  rep(each = 2) %>%
+  tail(-1) %>%
+  paste(elections_1962[5, ], sep = "_")
+
+names(elections_1962)[1] <- "region"
+
+
+
+# The first five rows of the dataset used the be the column names
+# We do not need them anymore. Thus eliminate them
+elections_1962 <- tail(elections_1962, n = -5)
+
+
+
+
+
